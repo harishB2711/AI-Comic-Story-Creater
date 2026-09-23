95437f30b17fe596089cea105fada5668eca3383
harishB2711 <hharishnjr@gmail.com>
2026-09-23 11:42:28 +0530

    Delete ComicCraft-UltraFast - Copy/ComicCraft directory

--- ComicCraft-UltraFast - Copy/ComicCraft/static/js/app.js
+++
@@ -1,9 +0,0 @@
-function showLoading() {
-  const button = document.getElementById("generate-button");
-  const loading = document.getElementById("loading");
-  if (button) {
-    button.disabled = true;
-    button.textContent = "Creating comic…";
-  }
-  if (loading) loading.hidden = false;
-}
